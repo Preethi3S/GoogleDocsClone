c4be25c591563c6c138e2356ccf6adf7ce7946a5
preethi <smjpreethi@gmail.com>
2024-12-17 14:36:52 +0530

    Remove Error Code comments

--- client/src/TextEditor.jsx
+++ client/src/TextEditor.jsx
@@ -31,16 +31,6 @@ const TextEditor = () => {
     };
   }, []);
 
-/*   useEffect(() => {
-
-    if(socket != null || quill == null) return 
-    socket.once("load-document" , document => {
-      quill.setContents(document)
-      quill.enable()
-    })
-    socket.emit('get-document' , documentId)
-
-  } , [socket , quill , documentId]) */
 
   useEffect(() => {
     if (socket == null || quill == null) return;
